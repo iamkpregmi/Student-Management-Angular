05afc928890304880cf2d21935c713456f82d278
Krishna Prasad Regmi <krishna.regmi@sharajman.com>
2025-04-01 14:02:02 +0530

    project setup and components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: HomeComponent},
+    {path:"about", component: AboutComponent},
+    {path:"contact", component: ContactComponent},
+    {path:"user", component: UserComponent},
+    {path:"**", component: PageNotFoundComponent},
+];
